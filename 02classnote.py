5e119d2cfc7ae7621f61e3134e8f2cf3f74d5fc8
Bivek Thapa <beingbivek@gmail.com>
2024-06-18 18:49:25 +0545

    Day 2 Class Notes

---
+++ 02classnote.py
@@ -0,0 +1,39 @@
+# this is 2nd class notes
+# \n new line
+# \" text \" to print in double inverted commas
+# \t tab spacing
+# \\ to print \
+
+# print("bivek \t thapa \nvery new")
+# print("\"Ram Bahadur\"")
+# print("\'Handsome\'")
+
+# print("I am \'loving\' \"python\" Programming Language\"")
+
+# Variables: Container to store value.
+
+# Syntax:
+# variable_name = value
+
+name = "Bivek Thapa"
+print(name)
+
+num1 = 10
+num2 = 3
+
+sum = num1 + num2
+diff = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+mod = num1 % num2 # Mod is Remainder after division
+intdiv = num1 // num2 # Number before decimal point of Quotient after division
+cube = num1 ** 3
+
+print(f"Some calculations of {num1} and {num2}")
+print(f"Sum is {sum}")
+print(f"Diff is {diff}")
+print(f"Mul is {mul}")
+print(f"Div is {div}")
+print(f"Mod is {mod}")
+print(f"Intdiv is {intdiv}")
+print(f"cube of {num1} is {cube}")
