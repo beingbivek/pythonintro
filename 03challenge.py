79734145ba11f8a0bf93378fd8b3ce0716695f4d
Bivek Thapa <beingbivek@gmail.com>
2024-06-20 13:25:00 +0545

    Array and List and some datatypes with input from user day 3

---
+++ 03challenge.py
@@ -0,0 +1,10 @@
+# WAP to add your 5 friend names and addresses to the list and print all their name and address
+
+friend = ["Biplov", "Gaurav", "Abhiyan", "Vivek", "Arun"]
+friendaddress = ["Pathari", "Pathari", "Dharan", "Chitwan", "Resunga"]
+
+print(f"{friend[0]} lives in {friendaddress[0]}")
+print(f"{friend[1]} lives in {friendaddress[1]}")
+print(f"{friend[2]} lives in {friendaddress[2]}")
+print(f"{friend[3]} lives in {friendaddress[3]}")
+print(f"{friend[4]} lives in {friendaddress[4]}")
---
+++ 03classnote.py
@@ -0,0 +1,38 @@
+# container1 = "Milk"
+# container2 = "Water"
+
+# Put logic here to swap the output of containers
+
+# 1st method
+# container3 = container1
+# container1 = container2
+# container2 = container3
+
+# 2nd method
+# container1, container2 = container2, container1
+
+# End of Logic
+
+# print(f"container 1 contains {container1}")
+# print(f"container 2 contains {container2}")
+
+# Data types
+# 1. Numeric - int (eg 100,-32),float (eg 1.1,-315.2)
+# 2. String - "",''
+# 3. List/Array - stores multiple value inside same variable []
+
+# Class work day 3 Input from user
+
+# create a program that finds the lengh of fruit words
+
+# fruit = input("Enter fruit: ")
+# total_length = len(fruit)
+# print(f"Total characters in {fruit} fruit is {total_length}")
+
+# create a program that collects first, middle and last name and prints full name
+
+f_name = input("Enter First Name: ")
+m_name = input("Enter Middle Name: ")
+l_name = input("Enter Last Name: ")
+print(f"Your Full Name is: {f_name} {m_name} {l_name}")
+print(f"Total Characters are: {len(f_name + m_name + l_name)}")
