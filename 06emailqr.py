9b2999f3a56618bcc3f29f8fed054c78c5b25e06
Bivek Thapa <beingbivek@gmail.com>
2024-06-24 19:27:59 +0545

    Day 6 Send First Email with title and body QR Generator

--- 06emailqr.py
+++ 06emailqr.py
@@ -1,6 +1,6 @@
 import qrcode
 
-code = "mailto:beingbivek@gmail.com?subject=To apply for current vacnacies&body=Greetings, I would like to join your company"
+code = "mailto:beingbivek@gmail.com?subject=My First Email Using Python&body=Hello, This is my first email using Python"
 img = qrcode.make(code)
 type(img)  # qrcode.image.pil.PilImage
-img.save("applyemail.png")
+img.save("firstemailQR.png")
