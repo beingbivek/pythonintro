0af392475b9af0840787befda9f4c7f4c90179cf
Bivek Thapa <beingbivek@gmail.com>
2024-07-06 13:55:35 +0545

    Day 12 Challenge with set

--- 12challenge.py
+++ 12challenge.py
@@ -21,17 +21,28 @@ names = [
 
 # print(names[0][0])
 
+dept_names = {dept[1] for dept in names} # Set of Department Names
+# print(dept_names)
+
+for dept in dept_names:
+    print("\n----------------\n")  
+    print(f"{dept} Department") 
+    print("\n") 
+    for name in names:
+        if name[1] == dept:
+            print(name[0])
+
 # Find all form all departments
 dept_it = [dept[0] for dept in names if dept[1].lower() == 'it']
-dept_en = [dept[0] for dept in names if dept[1].lower() == 'engineering']
-dept_de = [dept[0] for dept in names if dept[1].lower() == 'designer']
-dept_ac = [dept[0] for dept in names if dept[1].lower() == 'account']
-dept_sm = [dept[0] for dept in names if dept[1].lower() == 'social media']
+# dept_en = [dept[0] for dept in names if dept[1].lower() == 'engineering']
+# dept_de = [dept[0] for dept in names if dept[1].lower() == 'designer']
+# dept_ac = [dept[0] for dept in names if dept[1].lower() == 'account']
+# dept_sm = [dept[0] for dept in names if dept[1].lower() == 'social media']
 
 
-# print("\nList of Employees in IT Departments\n")  
-# for dept in dept_it:
-#     print(dept)  
+print("\nList of Employees in IT Departments\n")  
+for dept in dept_it:
+    print(dept)  
 
 # print("\n----------------\n")  
 # for name in names:
@@ -39,30 +50,30 @@ dept_sm = [dept[0] for dept in names if dept[1].lower() == 'social media']
 # print([dept[0] for dept in names if dept[1].lower() == 'it'])
 # dept_it = [dept[0] for dept in names if dept[1].lower() == 'it']
 # print()
-print("List of Employees in all Departments\n") 
-print("\n----------------\n")  
-print("IT Department") 
-print("\n")  
-for name in dept_it:
-    print(name)
-print("\n----------------\n")  
-print("Accounting Department") 
-print("\n")  
-for name in dept_ac:
-    print(name)
-print("\n----------------\n")  
-print("Social Media Department") 
-print("\n")  
-for name in dept_sm:
-    print(name)
-print("\n----------------\n")  
-print("Design Department") 
-print("\n")  
-for name in dept_de:
-    print(name)
-print("\n----------------\n")  
-print("Engineering Department") 
-print("\n")  
-for name in dept_en:
-    print(name)
+# print("List of Employees in all Departments\n") 
+# print("\n----------------\n")  
+# print("IT Department") 
+# print("\n")  
+# for name in dept_it:
+#     print(name)
+# print("\n----------------\n")  
+# print("Accounting Department") 
+# print("\n")  
+# for name in dept_ac:
+#     print(name)
+# print("\n----------------\n")  
+# print("Social Media Department") 
+# print("\n")  
+# for name in dept_sm:
+#     print(name)
+# print("\n----------------\n")  
+# print("Design Department") 
+# print("\n")  
+# for name in dept_de:
+#     print(name)
+# print("\n----------------\n")  
+# print("Engineering Department") 
+# print("\n")  
+# for name in dept_en:
+#     print(name)
 
